1a039190926e7d7560674e37a49eae59140b4e70
Giovane Richard <giovanerichard@gmail.com>
2023-10-07 20:22:00 -0300

    Feat/ Initialization project whit Charkra ui, Formik and Yup

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <Box>Home</Box>;
+}
